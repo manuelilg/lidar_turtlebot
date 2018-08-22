a029173431c5976ef5788d461bd9472952f09f0b
Manuel Ilg <manuelilg@bluemail.ch>
2018-08-22 13:17:32 +0200

    change cartographer configuration for newest cartogarpher version

--- lidar_turtlebot_cartographer/configuration_files/turtlebot_vlp16_scan_2d_localisation.lua
+++ lidar_turtlebot_cartographer/configuration_files/turtlebot_vlp16_scan_2d_localisation.lua
@@ -1,6 +1,9 @@
 include "turtlebot_vlp16_scan_2d.lua"
 
-TRAJECTORY_BUILDER.pure_localization = true
+--TRAJECTORY_BUILDER.pure_localization = true
+TRAJECTORY_BUILDER.pure_localization_trimmer = {
+  max_submaps_to_keep = 3,
+}
 POSE_GRAPH.optimize_every_n_nodes = 20
 --options.published_frame = "map"
 
